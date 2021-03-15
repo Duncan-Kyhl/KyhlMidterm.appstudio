fb5750ad026f44cc869aa543864ee8ded596010d
Duncan-Kyhl <Duncan-Kyhl@users.noreply.github.com>
2021-03-15 14:26:46 -0500

    program1

---
+++ KyhlMidterm.appstudio/forms/animals/animals.js
@@ -0,0 +1,8 @@
+// Program 1: Animals
+
+let animal = prompt('Enter an animal: ')
+let myAnimals = ['dog', 'cat', 'horse', 'meerkat']
+
+myAnimals.push(animal.toLowerCase())
+
+console.log(`The last animal is a/an ${myAnimals[myAnimals.length-1]}.`)
